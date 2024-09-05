4b373728a7494bd0e23f0eb3ae15487c052eaabb
GitHub-LP <lp1931steam@gmail.com>
2024-09-05 18:12:01 +0800

    update

--- stage_1/01_python_basics/04_basic_operator.py
+++ stage_1/01_python_basics/04_basic_operator.py
@@ -23,3 +23,35 @@ mod_result = a % b
 # 幂
 power_result = a ** b
 
+# 比较运行符
+# 大于
+greater_than_result = a > b
+
+# 小于
+less_than_result = a < b
+
+# 大于等于
+greater_than_equal_result = a >= b
+
+# 小于等于
+less_than_equal_result = a <= b
+
+# 等于
+equal_result = a == b
+
+# 不等于
+not_equal_result = a != b
+
+# 逻辑运算符
+# 逻辑与
+1 and 0  # 0
+True and False  # False
+
+
+# 逻辑或
+0 or 1  # 1
+True or False # True
+
+# 逻辑非
+1 not in [0, 1]  # False
+not True  # False
