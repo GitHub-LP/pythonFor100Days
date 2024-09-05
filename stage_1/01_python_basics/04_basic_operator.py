84a7f7d410297aa8f5f59457bd4244b696d99d57
GitHub-LP <lp1931steam@gmail.com>
2024-09-05 18:10:45 +0800

    update

--- stage_1/01_python_basics/04_basic_operator.py
+++ stage_1/01_python_basics/04_basic_operator.py
@@ -8,15 +8,18 @@ add_result = a + b
 # 减法
 subtract_result = a - b
 
-#乘法
+# 乘法
 multiply_result = a * b
 
-#除法
+# 除法
 divide_result = a / b
 
-#整除
+# 整除
 floor_div_result = a // b
 
-#取模
+# 取模
 mod_result = a % b
+
+# 幂
+power_result = a ** b
 
