f9ab1a6c3f9a37285ba2bfc3baea64197db436cd
GitHub-LP <lp1931steam@gmail.com>
2024-09-23 13:52:55 +0800

    update LICENSE file by LP

--- stage_1/02_controlling_processes_and_data_structures/08_select_statement.py
+++ stage_1/02_controlling_processes_and_data_structures/08_select_statement.py
@@ -3,8 +3,8 @@
 Author: GitHub-LP
 Date: 2024-09-13 17:51:36
 LastEditors: LP
-LastEditTime: 2024-09-13 17:51:43
-FilePath: \pythonFor100Days\stage_1\01_python_basics\08_select_statement.py
+LastEditTime: 2024-09-23 13:52:38
+FilePath: \pythonFor100Days\stage_1\02_controlling_processes_and_data_structures\08_select_statement.py
 Description: 
 Copyright (c) 2024 by GitHub-LP, All Rights Reserved.
 '''
